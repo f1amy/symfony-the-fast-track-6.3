1fb56df77b5ef4c8470589e2f8c3d4db4d3701dc
Flamy <27062841+f1amy@users.noreply.github.com>
2023-07-15 02:27:11 +0500

    Implement pagination for comments
    We have updated the `ConferenceController` and `CommentRepository` to handle pagination. The 'show' method now supports a page offset parameter, which is used to retrieve comments for a particular conference. `getCommentPaginator()` has been added to `CommentRepository`, which retrieves comments based on the offset and a constant page limit. The conference twig template was also modified to display "Next" or "Previous" links based on the comment count and current offset. This change improves usability by not loading all comments at once and allows users to navigate through comments easily.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -6,6 +6,7 @@ use App\Entity\Conference;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,13 +23,16 @@ class ConferenceController extends AbstractController
     }
 
     #[Route('/conference/{id}', name: 'conference')]
-    public function show(Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
     {
-        $comments = $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']);
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $commentPaginator = $commentRepository->getCommentPaginator($conference, $offset);
 
         return $this->render('conference/show.html.twig', [
             'conference' => $conference,
-            'comments' => $comments,
+            'comments' => $commentPaginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($commentPaginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
         ]);
     }
 }
