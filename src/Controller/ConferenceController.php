456ad7e328670fb8739fb16b8957efc3c90c1482
Flamy <27062841+f1amy@users.noreply.github.com>
2023-07-15 02:16:43 +0500

    Add individual conference view with comments
    Added an individual view for each conference under the 'conference' route which shows the details of the conference along with the comments. Fetching of all conferences and comments from the repository is now done within the controller, enhancing code readability and modularity.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,19 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
+    public function index(ConferenceRepository $conferenceRepository): Response
     {
+        $conferences = $conferenceRepository->findAll();
+
         return $this->render('conference/index.html.twig', [
-            'conferences' => $conferenceRepository->findAll(),
+            'conferences' => $conferences,
+        ]);
+    }
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Conference $conference, CommentRepository $commentRepository): Response
+    {
+        $comments = $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']);
+
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $comments,
         ]);
     }
 }
