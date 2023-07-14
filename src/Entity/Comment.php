0b27c1ebc5cdcc832f1a72308870fd451c0bafb0
Flamy <27062841+f1amy@users.noreply.github.com>
2023-07-15 01:22:45 +0500

    Add __toString method to Comment and Conference entities
    Added a __toString method to both the Comment and Conference entities for improved UX and debugging. The __toString method on Comment returns the email while the one on Conference returns a formatted string of the city and year.

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -33,6 +33,11 @@ class Comment
     #[ORM\Column(length: 255)]
     private ?string $photoFilename = null;
 
+    public function __toString(): string
+    {
+        return (string) $this->email;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
