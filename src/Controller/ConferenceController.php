087ff33393c9acb20a852bed8ac89bb529aeda5c
Flamy <27062841+f1amy@users.noreply.github.com>
2023-07-15 18:49:53 +0500

    Add photo uploading feature to comments
    Added a feature allowing users to upload photos along with their comments. This feature can add more depth and context to comment threads. It also includes form validation to ensure that only valid images are uploaded. This required changes to the .gitignore file, adding a new path to store the user-uploaded images. The services.yaml file was modified to include the path of the photos as a parameter. Corresponding changes were also made to the ConferenceController to handle photo uploads and updates to the comment entity and form to include photo. In addition, the show.html.twig template was updated to include the photo upload form field.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,16 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentType;
 use App\Repository\CommentRepository;
-use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+    ) {
+    }
+
     #[Route('/', name: 'homepage')]
     public function index(): Response
     {
@@ -19,8 +27,33 @@ class ConferenceController extends AbstractController
     }
 
     #[Route('/conference/{slug}', name: 'conference')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
-    {
+    public function show(
+        Request $request,
+        Conference $conference,
+        CommentRepository $commentRepository,
+        #[Autowire('%app.photo_dir%')] string $photoDir,
+    ): Response {
+        $comment = new Comment();
+        $commentForm = $this->createForm(CommentType::class, $comment);
+
+        $commentForm->handleRequest($request);
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+            $comment->setConference($conference);
+
+            if ($photo = $commentForm['photo']->getData()) {
+                $randomString = bin2hex(random_bytes(6));
+                $filename = sprintf('%s.%s', $randomString, $photo->guessExtension());
+
+                $photo->move($photoDir, $filename);
+                $comment->setPhotoFilename($filename);
+            }
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
         $offset = max(0, $request->query->getInt('offset', 0));
         $commentPaginator = $commentRepository->getCommentPaginator($conference, $offset);
 
@@ -32,6 +65,7 @@ class ConferenceController extends AbstractController
             'comments' => $commentPaginator,
             'previous' => $previous,
             'next' => $next,
+            'commentForm' => $commentForm,
         ]);
     }
 }
