56686705ccf42146462770b31aa3c1f68fcd70b8
Flamy <27062841+f1amy@users.noreply.github.com>
2023-07-17 03:03:01 +0500

    Refactor translation usage and adjust tests accordingly
    The translation method in the conference display template was updated to use a more appropriate syntax. The translation key for the comment count was also renamed for clarity. Test routes have been updated accordingly and the messages are now localized correctly for English and Russian languages. These changes are made to improve consistency across codebase and enhance localization.

--- tests/Functional/Controller/ConferenceControllerTest.php
+++ tests/Functional/Controller/ConferenceControllerTest.php
@@ -12,7 +12,7 @@ class ConferenceControllerTest extends WebTestCase
     public function testIndex(): void
     {
         $client = static::createClient();
-        $client->request('GET', '/');
+        $client->request('GET', '/en/');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
@@ -22,7 +22,7 @@ class ConferenceControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $client->request('GET', '/conference/amsterdam-2019');
+        $client->request('GET', '/en/conference/amsterdam-2019');
 
         $email = 'me@automat.ed';
         $client->submitForm('Submit', [
@@ -47,7 +47,7 @@ class ConferenceControllerTest extends WebTestCase
     public function testConferencePage(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/en/');
 
         $this->assertCount(2, $crawler->filter('h4'));
 
@@ -56,6 +56,6 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertPageTitleContains('Amsterdam');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
-        $this->assertSelectorExists('div:contains("There are 1 comments")');
+        $this->assertSelectorExists('div:contains("There is one comment")');
     }
 }
