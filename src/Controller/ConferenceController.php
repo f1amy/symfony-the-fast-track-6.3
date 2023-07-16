30b5ffb9a3c92829d8721c6fa17a346c6ce425b6
Flamy <27062841+f1amy@users.noreply.github.com>
2023-07-16 20:47:30 +0500

    Replace SpamChecker with NotifierInterface in ConferenceController
    The SpamChecker service was swapped out with the NotifierInterface in the ConferenceController. This change brings in better user feedback functionality. When a comment form is submitted, a notification is generated to inform users about the status of their submission. Either they’re thanked for the feedback or notified about some issues with the form submission, depending on the form state. This makes the application more user-friendly as users get immediate responses on their actions.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,13 +8,14 @@ use App\Form\CommentType;
 use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
-use App\Service\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
@@ -53,7 +54,7 @@ class ConferenceController extends AbstractController
         Request $request,
         Conference $conference,
         CommentRepository $commentRepository,
-        SpamChecker $spamChecker,
+        NotifierInterface $notifier,
         #[Autowire('%app.photo_dir%')] string $photoDir,
     ): Response {
         $comment = new Comment();
@@ -83,7 +84,22 @@ class ConferenceController extends AbstractController
             $commentMessage = new CommentMessage($comment->getId(), $context);
             $this->bus->dispatch($commentMessage);
 
+            $feedbackNotification = new Notification(
+                'Thank you for the feedback; your comment will be posted after moderation.',
+                ['browser']
+            );
+            $notifier->send($feedbackNotification);
+
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
+        if ($commentForm->isSubmitted()) {
+            $submissionErrorNotification = new Notification(
+                'Can you check your submission? There are some problems with it.',
+                ['browser']
+            );
+
+            $notifier->send($submissionErrorNotification);
         }
 
         $offset = max(0, $request->query->getInt('offset', 0));
